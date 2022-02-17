5ba326529c9d5988af5ef61b8a2b9ee5c413832c
Komsakorn Jiarawish <komsakorn.j@hotmail.com>
2022-02-17 14:12:00 +0700

    finalize

---
+++ intro-to-js/english_in_name.js
@@ -0,0 +1,11 @@
+function askMyName(){
+    const name = prompt("enter you name") // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
+    if (name != null) {
+       document.getElementById("q-name").innerHTML = "My name is " + name;
+    }
+ }
+ 
+ function helloWorld() {
+ const h1message = document.getElementById("q-name").innerHTML// ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+    alert(h1message + '_All of my bad pics been all my best ones.-(Doja cat)')
+ }
